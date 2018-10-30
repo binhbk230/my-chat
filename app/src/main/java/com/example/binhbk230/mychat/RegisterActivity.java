15f86d3e5773c7dbc6cf62c71f8806e8a8ac4d46
van binh <vanbinhhut@gmail.com>
2018-10-30 11:23:45 +0700

    Create new account
    Create new account

--- app/src/main/java/com/example/binhbk230/mychat/RegisterActivity.java
+++ app/src/main/java/com/example/binhbk230/mychat/RegisterActivity.java
@@ -1,21 +1,94 @@
 package com.example.binhbk230.mychat;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private EditText registerUserName;
+    private EditText registerEmail;
+    private EditText registerUserPassword;
+    private Button createAccountButton;
+    private ProgressDialog mProgressDialog;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+
         mToolbar = (Toolbar) findViewById(R.id.register_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("SignUp");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mProgressDialog = new ProgressDialog(this);
+        registerEmail = (EditText) findViewById(R.id.register_email);
+        registerUserName = (EditText) findViewById(R.id.register_name);
+        registerUserPassword = (EditText) findViewById(R.id.register_password);
+
+        createAccountButton = (Button) findViewById(R.id.create_account_button);
+
+        createAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = registerUserName.getText().toString();
+                String email = registerEmail.getText().toString();
+                String password = registerUserPassword.getText().toString();
+
+                registerAccount(name, email, password);
+            }
+        });
+    }
+
+    private void registerAccount(String name, String email, String password) {
+        if(TextUtils.isEmpty(name)) {
+            Toast.makeText(RegisterActivity.this, "Please write your name", Toast.LENGTH_LONG).show();
+        }
+        if(TextUtils.isEmpty(email)) {
+            Toast.makeText(RegisterActivity.this, "Please write your email", Toast.LENGTH_LONG).show();
+        }
+        if(TextUtils.isEmpty(password)) {
+            Toast.makeText(RegisterActivity.this, "Please write your password", Toast.LENGTH_LONG).show();
+        }
+
+        else {
+            mProgressDialog.setTitle("Creating new account");
+            mProgressDialog.setMessage("Please wait while we creating the new account");
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()) {
+                        Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(mainIntent);
+                        finish();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Error Occured, Try Again", Toast.LENGTH_LONG).show();
+                    }
+
+                    mProgressDialog.dismiss();
+                }
+            });
+        }
     }
 }
