76fd2cf1788c3ccddef9988253e454f2b5df7ec0
van binh <vanbinhhut@gmail.com>
2018-11-01 20:46:10 +0700

    Update fragment
    Update fragment

--- app/src/main/java/com/example/binhbk230/mychat/LoginActivity.java
+++ app/src/main/java/com/example/binhbk230/mychat/LoginActivity.java
@@ -1,12 +1,27 @@
 package com.example.binhbk230.mychat;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private EditText mEmail;
+    private EditText mPassword;
+    private Button loginButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +32,34 @@ public class LoginActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Login");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mAuth = FirebaseAuth.getInstance();
+        mEmail = (EditText) findViewById(R.id.login_email);
+        mPassword = (EditText) findViewById(R.id.login_password);
+
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Wrong username or Password, Try Again", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/binhbk230/mychat/MainActivity.java
+++ app/src/main/java/com/example/binhbk230/mychat/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.binhbk230.mychat;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,11 +18,22 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private Toolbar mToolbar;
 
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsPagerAdapter myTabsPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
+        //Tabs for Main Activity
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsPagerAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsPagerAdapter);
+        myTabLayout = (TabLayout) findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
 
         mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
